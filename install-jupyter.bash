20e78625e0007c055ee1fa158011e86f2f652c70
YuviPanda <yuvipanda@gmail.com>
2015-12-14 18:46:27 -0800

    Split installing into more stages

--- install-jupyter.bash
+++ install-jupyter.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+virtualenv -p python 3 "$1"
+source "$1/bin/activate"
+
 pip install jupyter
 
 R -f install-kernel.r
---
+++ install-packages.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install --yes libzmq3-dev libzmq3 r-base
